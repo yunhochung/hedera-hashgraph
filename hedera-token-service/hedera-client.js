de5db6ec50e0cf16e4db5b85e8a0afbb4925a59b
Yunho Chung <yunho.chung@outlook.com>
2020-12-12 21:53:13 +0900

    소스 리팩토링 했다.

--- hedera-token-service/hedera-client.js
+++ hedera-token-service/hedera-client.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const { Client } = require('@hashgraph/sdk');
 
-// console.log('process.env.ACCOUNT_ID: ', process.env.ACCOUNT_ID);
-
 const HederaClient = new Client({
     // Testnet Proxy BY YH
     network: { '13.124.85.3:80': '0.0.3' },
@@ -10,8 +8,6 @@ const HederaClient = new Client({
 
     // Previewnet
     // network: { '35.231.208.148:50211': '0.0.3'},
-    // Previenet Proxy BY YH
-    // network: { '13.124.85.3:443': '0.0.3'}
     operator: {
         accountId: process.env.ACCOUNT_ID,
         privateKey: process.env.PRIVATE_KEY
